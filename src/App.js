89fd09c1ffab36afc0016b9c75e5899e29b82de5
tedogls <tedogulisa1996@gmail.com>
2025-01-05 21:14:36 +0100

    first-commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import TodoList from "./features/todos/TodoList";
+
+function App() {
+  return (
+    <div>
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/features/todos/TodoList.js
@@ -0,0 +1,5 @@
+function TodoList() {
+  return <div>Hello</div>;
+}
+
+export default TodoList;
